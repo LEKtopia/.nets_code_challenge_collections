0683010aef6999d4f4d41ba681fb9cdb8d572cd3
LEKtopia <24890955+LEKtopia@users.noreply.github.com>
2019-01-20 21:59:46 -0600

    Initial commit - add files via upload

---
+++ CodeChallengeCollections_lkastel1/CodeChallengeCollections_lkastel1/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNameEntered = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.lstNameList = New System.Windows.Forms.ListBox()
+        Me.btnClearList = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(63, 110)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(121, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Philosopher Name"
+        '
+        'txtNameEntered
+        '
+        Me.txtNameEntered.Location = New System.Drawing.Point(33, 126)
+        Me.txtNameEntered.Name = "txtNameEntered"
+        Me.txtNameEntered.Size = New System.Drawing.Size(185, 20)
+        Me.txtNameEntered.TabIndex = 1
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(233, 94)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(76, 40)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "&Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(233, 140)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(76, 38)
+        Me.btnRemove.TabIndex = 3
+        Me.btnRemove.Text = "&Remove"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'lstNameList
+        '
+        Me.lstNameList.AllowDrop = True
+        Me.lstNameList.FormattingEnabled = True
+        Me.lstNameList.Location = New System.Drawing.Point(324, 48)
+        Me.lstNameList.Name = "lstNameList"
+        Me.lstNameList.Size = New System.Drawing.Size(197, 147)
+        Me.lstNameList.TabIndex = 4
+        '
+        'btnClearList
+        '
+        Me.btnClearList.Location = New System.Drawing.Point(324, 201)
+        Me.btnClearList.Name = "btnClearList"
+        Me.btnClearList.Size = New System.Drawing.Size(197, 23)
+        Me.btnClearList.TabIndex = 5
+        Me.btnClearList.Text = "Clear the List"
+        Me.btnClearList.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(206, 243)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(153, 36)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(382, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Philosopher List"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(552, 306)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClearList)
+        Me.Controls.Add(Me.lstNameList)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtNameEntered)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Code Challenge Collections"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNameEntered As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents lstNameList As ListBox
+    Friend WithEvents btnClearList As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ CodeChallengeCollections_lkastel1/CodeChallengeCollections_lkastel1/Form1.vb
@@ -0,0 +1,37 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class Form1
+    Dim nameEntered As String
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        ' Prevent Duplicates in the listBox
+        If Not lstNameList.Items.Contains(txtNameEntered.Text) Then
+            lstNameList.Items.Add(txtNameEntered.Text)
+        Else
+            MessageBox.Show("This philosopher is already listed.  Try again.")
+        End If
+
+        ' Automatically clear the textBox
+        txtNameEntered.Clear()
+
+        ' Reset focus
+        txtNameEntered.Focus()
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        ' Remove the selected name.
+        lstNameList.Items.Remove(lstNameList.SelectedItem)
+    End Sub
+
+    Private Sub btnClearList_Click(sender As Object, e As EventArgs) Handles btnClearList.Click
+        ' Clear the list.
+        lstNameList.Items.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form.
+        Me.Close()
+    End Sub
+
+End Class
